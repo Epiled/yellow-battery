35951677e7926229674fe17b3d3dcbe4464d11e1
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2022-08-01 23:10:14 -0300

    Refatoraçao e aplicação de DRY no banner.js

--- src/assets/js/banner.js
+++ src/assets/js/banner.js
@@ -1,20 +1,41 @@
 let slideIndex = 0;
 let slides = document.querySelectorAll("[data-slide]");
 let dots = document.querySelectorAll("[data-dot]");
+let timer;
 
 function showSlides() {
   let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}   
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" banner__dot--active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " banner__dot--active";
-  setTimeout(showSlides, 5000);
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  resetIndex(slideIndex);
+}
+
+function plusSlides(position) {
+  clearTimeout(timer);
+  slideIndex +=position;
+  if (slideIndex> slides.length) {slideIndex = 1}
+  else if(slideIndex<1){slideIndex = slides.length}
+  resetIndex(slideIndex);
+}
+
+function currentSlide(index) {
+  clearTimeout(timer);
+  if (index> slides.length) {index = 1}
+  else if(index<1){index = slides.length}
+  slideIndex = index;
+  resetIndex(slideIndex);
+}
+
+function resetIndex(index) {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+ }
+ for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" banner__dot--active", "");
+   }
+   slides[index-1].style.display = "block";  
+   dots[index-1].className += " banner__dot--active";
+   timer = setTimeout(showSlides, 4000);
 }
 
 showSlides();
