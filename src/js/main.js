7b662c720e500dc49df27584ad1879fb81a9b2e5
Felipe De Andrade <felipe.deam98@gmail.com>
2026-06-16 03:42:29 -0300

    style: format codebase with prettier

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 const iconeHamburguer = document.querySelector("[data-iconeHamburguer]");
 const menuLista = document.querySelector("[data-menuLista]");
 
-iconeHamburguer.addEventListener("click", function(){
-    menuLista.classList.toggle('menu__lista--show');
+iconeHamburguer.addEventListener("click", function () {
+  menuLista.classList.toggle("menu__lista--show");
 });
 
-menuLista.addEventListener("click", function(){
-    menuLista.classList.toggle('menu__lista--show');
+menuLista.addEventListener("click", function () {
+  menuLista.classList.toggle("menu__lista--show");
 });
-  
